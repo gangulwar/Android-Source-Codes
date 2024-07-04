a00ac93a980a6d118ed127a25a0cc3271ac68a67
Aarsh Gangulwar <aarshgangulwar23@gmail.com>
2024-07-04 12:59:06 +0530

    add MyApp

---
+++ MyApp/app/src/main/java/com/btn/hsn/myapp/IntroActivity.java
@@ -0,0 +1,36 @@
+package com.btn.hsn.myapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        ProgressBar progressBar= (ProgressBar) findViewById(R.id.intro_progress);
+        progressBar.getIndeterminateDrawable().setColorFilter(getResources().getColor(R.color.colorPrimaryLight), android.graphics.PorterDuff.Mode.SRC_IN);
+        getSupportActionBar().hide();
+        Timer myTimer = new Timer();
+        myTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // If you want to modify a view in your Activity
+                IntroActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(IntroActivity.this,MainActivity.class));
+                        finish();
+
+                    }
+                });
+            }
+        }, 3000);
+    }
+}
